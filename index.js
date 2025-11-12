249343195cdad0929875c0c65dbbefabc2913d60
Kazi Fabiha Golam Liya <151756377+KaziFabihaLiya@users.noreply.github.com>
2025-11-13 00:56:50 +0600

    Get, Post Implemented

--- index.js
+++ index.js
@@ -1,46 +1,66 @@
-const express = require('express')
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = 5000;
 
 //Middleware
 app.use(cors());
 app.use(express.json());
 
-//const uri = "mongodb+srv://StudyMateAdmin:Em9TJ5RZFm6Txu1M@cluster0.8ev9gxa.mongodb.net/?appName=Cluster0";
+//const uri ="mongodb+srv://StudyMateAdmin:Em9TJ5RZFm6Txu1M@cluster0.8ev9gxa.mongodb.net/?appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+    const DB = client.db("StudyMateDB");
+    const profileCol = DB.collection("partnerProfiles");
+
+    app.get("/AllPartnerProfile", async (req, res) => {
+      const result = await profileCol.find().toArray();
+      console.log(result);
+      res.send(result);
+    });
+    app.post("/createProfile", async (req, res) => {
+      const data = req.body;
+      const result = await profileCol.insertOne(data);
+      console.log("created prof",result);
+      res.send(result);
+    });
+
+    app.get("/profileDetails/:id", async (req, res) => {
+        console.log(`Fetching profile for ID: ${req.params.id}`); // Debug
+        const { id } = req.params;
+        const objectId = new ObjectId(id);
+        const result = await profileCol.findOne({ _id: objectId });
+        res.send({
+          success: true,
+          result,
+        });
+
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello StudyMate!')
-})
-
-
+app.get("/", (req, res) => {
+  res.send("Hello StudyMate!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
